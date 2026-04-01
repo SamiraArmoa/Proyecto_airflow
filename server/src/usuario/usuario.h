189f38d34b2eb9b0fb61b0d974e93f6a824798b4
Samira Armoa <s.armoa@opendeusto.es>
2026-04-01 20:34:52 +0200

    main.c con el menu y usuario.c y .h 🌈
    gcc main.c .\usuario\usuario.c -o main.exe

---
+++ server/src/usuario/usuario.h
@@ -0,0 +1,24 @@
+/*
+ * cliente.h
+ *
+ *  Created on: 1 abr 2026
+ *      Author: s.armoa
+ */
+
+#ifndef SERVER_SRC_USUARIO_USUARIO_H_
+#define SERVER_SRC_USUARIO_USUARIO_H_
+
+typedef struct {
+    int id;
+    char nombre[100];
+    char email[100];
+    int telefono;
+    char contrasena[100];
+} Usuario;
+
+int crearUsuario();
+int eliminarUsuario();
+int actualizarUsuario();
+//int imprimirUsuarios();
+
+#endif /* SERVER_SRC_USUARIO_USUARIO_H_ */
