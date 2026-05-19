618fc9193a82772489cf00a0cb5bda949016b388
Oier Artabe <oier.artabe@opendeusto.es>
2026-05-19 13:29:48 +0200

    Crear pasajero.h y .cpp

---
+++ client/src/pasajero/Pasajero.cpp
@@ -0,0 +1,8 @@
+/*
+ * Pasajero.cpp
+ *
+ *  Created on: 19 may 2026
+ *      Author: oier.artabe
+ */
+
+
---
+++ client/src/pasajero/Pasajero.h
@@ -0,0 +1,13 @@
+/*
+ * Pasajero.h
+ *
+ *  Created on: 19 may 2026
+ *      Author: oier.artabe
+ */
+
+#ifndef CLIENT_SRC_PASAJERO_PASAJERO_H_
+#define CLIENT_SRC_PASAJERO_PASAJERO_H_
+
+
+
+#endif /* CLIENT_SRC_PASAJERO_PASAJERO_H_ */
