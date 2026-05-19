2913f0049c31e18c2f7cb5a220b2e272bbe98e40
Oier Artabe <oier.artabe@opendeusto.es>
2026-05-19 13:29:20 +0200

    Crear controlador.h y .cpp

---
+++ client/src/usuario/controlador.h
@@ -0,0 +1,13 @@
+/*
+ * controlador.h
+ *
+ *  Created on: 19 may 2026
+ *      Author: oier.artabe
+ */
+
+#ifndef CLIENT_SRC_USUARIO_CONTROLADOR_H_
+#define CLIENT_SRC_USUARIO_CONTROLADOR_H_
+
+
+
+#endif /* CLIENT_SRC_USUARIO_CONTROLADOR_H_ */
