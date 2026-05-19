ae967677af11f6adcb728b12b0ca7e35d7347762
Oier Artabe <oier.artabe@opendeusto.es>
2026-05-19 13:29:32 +0200

    Crear clientSocket.h y .cpp

---
+++ client/src/usuario/clientSocket.h
@@ -0,0 +1,13 @@
+/*
+ * clientSocket.h
+ *
+ *  Created on: 19 may 2026
+ *      Author: oier.artabe
+ */
+
+#ifndef CLIENT_SRC_USUARIO_CLIENTSOCKET_H_
+#define CLIENT_SRC_USUARIO_CLIENTSOCKET_H_
+
+
+
+#endif /* CLIENT_SRC_USUARIO_CLIENTSOCKET_H_ */
